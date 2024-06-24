8cce358bd0cb0ef2ab0a21b38c0c9a91ae0e9b65
Gustavo <envolti.gustavo.peron@redeunifique.com.br>
2024-06-24 06:24:54 -0300

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var slider1 = new KeenSlider("#carousel-icons", {
-    loop: false,
-    mode: "free",
-    slides: {
-      perView: 4.5,
-      spacing: 14,
-    },
-  });
+  if (typeof KeenSlider !== "undefined") {
+    var slider1 = new KeenSlider("#carousel-icons", {
+      loop: false,
+      mode: "free",
+      slides: {
+        perView: 4.5,
+        spacing: 14,
+      },
+    });
 
-  var slider2 = new KeenSlider("#carousel-textos", {
-    loop: false,
-    mode: "free",
-    slides: {
-      perView: 1.5,
-      spacing: 14,
-    },
-  });
+    var slider2 = new KeenSlider("#carousel-textos", {
+      loop: false,
+      mode: "free",
+      slides: {
+        perView: 1.5,
+        spacing: 14,
+      },
+    });
 
-  var slider3 = new KeenSlider("#carousel-discover", {
-    loop: false,
-    mode: "free",
-    slides: {
-      perView: 1.8,
-      spacing: 14,
-    },
-  });
+    var slider3 = new KeenSlider("#carousel-discover", {
+      loop: false,
+      mode: "free",
+      slides: {
+        perView: 1.8,
+        spacing: 14,
+      },
+    });
+  }
 });
