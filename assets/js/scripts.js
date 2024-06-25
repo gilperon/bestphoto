75951e55acbc1e6a10a515cb9f18393da01ecafc
Gustavo <envolti.gustavo.peron@redeunifique.com.br>
2024-06-25 10:52:30 -0300

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+// Page Rate photo
 $(document).ready(function () {
   let clickCounter = 1;
   $slide = 1;
@@ -74,3 +75,95 @@ $(document).ready(function () {
     clickCounter = 1;
   });
 });
+
+// Page discover
+$(document).ready(function () {
+  var $currentGridItem;
+  var imageDataUrl;
+  var cropper;
+
+  $(".grid-add-item").on("click", function () {
+    $currentGridItem = $(this);
+    $("#file-upload").click();
+  });
+
+  $("#file-upload").on("change", function (event) {
+    var file = event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        imageDataUrl = e.target.result;
+        $("#uploaded-photo").attr("src", imageDataUrl);
+
+        var myModal = new bootstrap.Modal(
+          document.getElementById("photoModal"),
+          {
+            keyboard: false,
+          }
+        );
+        myModal.show();
+
+        myModal._element.addEventListener("shown.bs.modal", function () {
+          cropper = new Cropper(document.getElementById("uploaded-photo"), {
+            aspectRatio: 1,
+            viewMode: 1, // Ensure the cropped area respects container's boundaries
+            autoCropArea: 0.8, // 80% of the visible area
+            movable: true,
+            zoomable: true,
+            rotatable: true,
+            scalable: true,
+          });
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+  $("#save-button").on("click", function () {
+    if (cropper) {
+      var croppedCanvas = cropper.getCroppedCanvas({
+        width: 100,
+        height: 100,
+      });
+
+      var croppedImageDataUrl = croppedCanvas.toDataURL("image/png");
+      $currentGridItem.css({
+        "background-image": "url(" + croppedImageDataUrl + ")",
+        "background-position":
+          -cropper.getData().x + "px " + -cropper.getData().y + "px",
+      });
+      $currentGridItem.find(".icon-plus").hide();
+
+      var myModalEl = document.getElementById("photoModal");
+      var modal = bootstrap.Modal.getInstance(myModalEl);
+      modal.hide();
+
+      cropper.destroy();
+      cropper = null;
+    }
+  });
+
+  $("#photoModal").on("hidden.bs.modal", function () {
+    if (cropper) {
+      cropper.destroy();
+      cropper = null;
+    }
+  });
+});
+// $(document).ready(function () {
+//   $(".grid-add-item").on("click", function () {
+//     $("#file-upload").click();
+//   });
+
+//   $("#file-upload").on("change", function (event) {
+//     var reader = new FileReader();
+//     reader.onload = function (e) {
+//       $("#uploaded-photo").attr("src", e.target.result);
+//       var photoModal = new bootstrap.Modal(
+//         document.getElementById("photoModal")
+//       );
+//       photoModal.show();
+//     };
+//     reader.readAsDataURL(event.target.files[0]);
+//   });
+// });
