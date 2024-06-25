d4ac768c4294ece846a16d5fbcbf2b259df3ace2
Gustavo <envolti.gustavo.peron@redeunifique.com.br>
2024-06-25 08:39:12 -0300

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,3 +28,49 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+$(document).ready(function () {
+  let clickCounter = 1;
+  $slide = 1;
+  $(".grid-photos-item").click(function () {
+    if (clickCounter <= 4) {
+      var dataValue = $(this).data("position");
+      if ($(this).find(".ranked-number").length > 0) {
+        $(".grid-photos-item").children(".ranked-number").remove();
+        $(this).append('<div class="ranked-number expandOpen">1</div>');
+        clickCounter = 2;
+      } else {
+        $(this).append(
+          '<div class="ranked-number expandOpen">' + clickCounter + "</div>"
+        );
+        if (clickCounter == 4) {
+          $("#icon-cash").addClass("hatch");
+          let currentBalance = $("#balance").text();
+          let balanceNumber = parseFloat(currentBalance);
+          balanceNumber += 0.001;
+          $("#balance").text(balanceNumber.toFixed(3));
+          setTimeout(function () {
+            $(`#slide-${$slide}`).hide();
+            $(`#slide-${$slide + 1}`)
+              .addClass("slideSmoothRight")
+              .show();
+            $(".grid-photos-item").children(".ranked-number").remove();
+            $slide++;
+          }, 600);
+          clickCounter = 1;
+          return;
+        }
+        clickCounter++;
+      }
+    }
+  });
+  $("#next-slide").click(function () {
+    $(`#slide-${$slide}`).hide();
+    $(`#slide-${$slide + 1}`)
+      .addClass("slideSmoothRight")
+      .show();
+    $(".grid-photos-item").children(".ranked-number").remove();
+    $slide++;
+    clickCounter = 1;
+  });
+});
